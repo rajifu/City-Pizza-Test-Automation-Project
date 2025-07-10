18b0f9d01d590e2e38001398edd70c748994ee09
Rajeswari <rajeswariavula25@gmail.com>
2025-07-10 17:07:31 +0200

    report enhancement

--- src/helper/report/report.ts
+++ src/helper/report/report.ts
@@ -4,6 +4,15 @@ import * as os from "os";
 const machineName = os.hostname();
 const platformName = os.platform();
 const platformVersion = os.release();
+const { execSync } = require('child_process');
+const timestamp = new Date().toLocaleString();
+let branchName = 'unknown';
+try {
+  branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+  console.log(`🚀 [${timestamp}] Collected Git branch name successfully: ${branchName}`);
+} catch (error) {
+  console.error('❌ Failed to get Git branch name:', error.message);
+}
 report.generate({
   jsonDir: "test-results",
   reportPath: "test-results/reports/",
@@ -29,6 +38,9 @@ report.generate({
       { label: "Cycle", value: "Smoke-1" },
       { label: "Machine Name", value: machineName },
       { label: "Platform", value: platformName },
+      { label: 'Branch', value: branchName },
+        { label: 'Execution Time', value: new Date().toLocaleString() },
+        { label: 'Executed by', value: process.env.USER || process.env.USERNAME },
       { label: "Execution Start Time", value: new Date().toLocaleString() },
       { label: "Execution End Time", value: new Date().toLocaleString() },
     ],
